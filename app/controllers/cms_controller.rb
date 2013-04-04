dc7598d6eccd8eca64e14c662369ad8a479a7b8f
raven <aossowski@gmail.com>
2013-04-04 17:47:10 +0200

    set valid locale code bump version

--- app/controllers/cms_controller.rb
+++ app/controllers/cms_controller.rb
@@ -1,12 +1,12 @@
 class CmsController < ApplicationController
 
   before_filter do
-    set_locale(params[:locale])
     @available_locales = ActiveadminSelleoCms::Locale.enabled
+    set_locale(params[:locale])
   end
 
-  def set_locale(locale_code = :en)
-    I18n.locale = locale_code
+  def set_locale(locale_code)
+    I18n.locale = @available_locales.map(&:code).include?(locale_code.to_s.to_sym) ? locale_code : I18n.default_locale
   end
 
 end
