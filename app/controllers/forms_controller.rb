cd2ad22e54d20a86d6ca2e5f7d1f428e6cc61daa
raven <aossowski@gmail.com>
2013-07-08 13:59:40 +0200

    form builder

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -8,13 +8,25 @@ class FormsController < ApplicationController
   def download
     respond_to do |format|
       format.pdf do
-        render :pdf => @form.title
+        render :pdf => @form.title.parameterize
       end
     end
   end
 
   def deliver
-
+    respond_to do |format|
+      format.html do
+        pdf = WickedPdf.new.pdf_from_string(
+            render_to_string('download.pdf.erb')
+        )
+        pdf_path = File.join(ActiveadminSelleoCms::Form::PDF_PATH, "#{@form.title.parameterize}-#{Time.now.to_s.parameterize}.pdf")
+        File.open(pdf_path, 'wb') do |file|
+          file << pdf
+        end
+        ActiveadminSelleoCms::FormMailer.form_submission(pdf_path).deliver
+        render :nothing => true
+      end
+    end
   end
 
 end
--- app/models/activeadmin_selleo_cms/form.rb
+++ app/models/activeadmin_selleo_cms/form.rb
@@ -1,5 +1,7 @@
 module ActiveadminSelleoCms
   class Form < ActiveRecord::Base
+    PDF_PATH = Rails.root.join('public','system','pdf')
+
     has_many :questions, class_name: "FormQuestion", :order => "position"
 
     validates :title, presence: true
