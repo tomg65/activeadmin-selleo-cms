7ca9960e3fd32dc1c668607a493659e6f084640b
raven <aossowski@gmail.com>
2013-07-10 15:34:47 +0200

    form printing

--- app/mailers/activeadmin_selleo_cms/form_mailer.rb
+++ app/mailers/activeadmin_selleo_cms/form_mailer.rb
@@ -1,11 +1,9 @@
 module ActiveadminSelleoCms
   class FormMailer < ActionMailer::Base
-    default from: "web-form@jips.org"
-
     def form_submission(form, form_uuid)
       @form = form
       @form_uuid = form_uuid
-      mail(to: 'aossowski@gmail.com', subject: "New form submission")
+      mail(subject: "New form submission")
     end
   end
 end
